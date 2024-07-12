9c3018c772e11c2e564925d109516b32f9637350
Mehebul02 <mehebul2123@gmail.com>
2024-07-13 00:55:11 +0600

    Complited

--- src/ui/Banner.jsx
+++ src/ui/Banner.jsx
@@ -3,13 +3,13 @@ import banner from "../assets/images/Profile Background.png";
 import profile from "../assets/images/Profile.png";
 const Banner = () => {
   return (
-    <div className="">
+    <div className="bg-[#F1F1F1]">
       <div className=" relative">
-        <img src={banner} alt="banner" className="" />
+        <img src={banner} alt="banner" className="w-full" />
       </div>
-      <div className="max-w-screen-xl mx-auto p-4">
+      <div className="max-w-screen-xl mx-auto  p-4">
         {/* profile  */}
-        <div className=" w-1/2 flex flex-col lg:flex-row items-center justify-between">
+        <div className=" w-1/2  flex flex-col lg:flex-row items-center justify-between">
           <div className="  flex items-center gap-4">
             <img
               src={profile}
@@ -34,8 +34,8 @@ const Banner = () => {
         </div>
 
         {/*   */}
-        <div className="flex flex-col lg:flex-row justify-between">
-          <div className="w-1/2 my-16">
+        <div className="flex flex-col lg:flex-row justify-between ">
+          <div className="w-1/2 my-16 border p-4 rounded-lg">
             <h1 className="text-[26px] font-bodyFont font-bold">
               How Astrophotography changed my life
             </h1>
@@ -54,7 +54,21 @@ const Banner = () => {
               vulputate nisi. Integer in felis sed leo vestibulum venenatis.
               Suspendisse{" "}
             </p>
+            {/* link  */}
+            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-wrap gap-2">
+              <button className="text-blue-600 bg-white px-3 py-2 rounded-full">#photography</button>
+              <button className="text-blue-600 bg-white px-3 py-2 rounded-full">#astronomy</button>
+              <button className="text-blue-600 bg-white px-3 py-2 rounded-full">#astrophotography</button>
+              <button className="text-blue-600 bg-white px-3 py-2 rounded-full font-bold">+</button>
+            </div>
+   <button className="text-xl text-blue-600 font-bodyFont font-bold">Read More</button>
+            </div>
           </div>
+                   {/* video content  */}
+<div>
+asdfsdfd
+</div>
         </div>
       </div>
     </div>
